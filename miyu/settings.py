721c371be9269865415bd1f55810e422ce735231
nicefuture2016 <nicefuture@foxmail.com>
2019-06-29 23:50:45 +0800

    #collection

--- miyu/settings.py
+++ miyu/settings.py
@@ -129,8 +129,8 @@ USE_L10N = True
 USE_TZ = False
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 LOG_DIR = os.environ['LOGDIR']
 
 # 短信配置
